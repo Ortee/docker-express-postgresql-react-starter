fc1ffd04aac4769789d4478393aac67ec97de688
Mateusz <kontakt@mateuszoracz.pl>
2016-10-11 02:24:37 +0200

    linking frontend with backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const app = require('express')();
+const path = require('path');
+const express = require('express');
+const fallback = require('express-history-api-fallback');
+
+let root = path.join(__dirname,'/../');
+
+app.use(express.static(root));
+app.use(fallback('index.html', {root: root}));
 
 app.get('/', function (req, res) {
   console.log('~/server/index.js : SUCCES');
-  res.json({succes: true});
+  res.sendfile('index.html', {root: root })
 });
 
 module.exports = app;
