fc97dc2870353f43c8b431d6bc216865d27d817e
Mateusz <kontakt@mateuszoracz.pl>
2016-10-13 12:43:18 +0200

    404 Component

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -4,12 +4,13 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Main from './components/MainComponent/main';
 import Author from './components/AuthorComponent/author';
-
+import NotFound from './components/NotFoundComponent/notfound';
 const app = document.getElementById('app');
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}/>
     <Route path="author" name="author" component={Author}/>
+    <Route path='*' component={NotFound} />
   </Router>,
 app);
---
+++ frontend/js/components/NotFoundComponent/notfound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <section>
+        <h1>404 NOT FOUND</h1>
+      </section>
+    );
+  }
+}
